1d23b06fd86bec0477e85d37e44ae6b813635b3d
mohitverma6161 <utkarshmalik06@gmail.com>
2021-05-18 21:13:48 +0530

    basic js files

---
+++ scriptingFile.js
@@ -0,0 +1,112 @@
+
+
+// //var //let and ///const
+
+
+
+
+// let message;
+// message="lerning JS";
+
+// alert(message);
+
+
+// let user="Utkarsh";
+// let  age=25;
+// let height=180;
+
+
+// //Data Types 
+
+
+
+
+
+
+
+
+
+// let name="Utkarsh";
+// let x=5;
+// let value=true; 
+
+
+// //null 
+// let age=null;
+
+
+// //undefined
+// let newName;
+
+// alert(newName);
+
+
+
+// var user={name:"Utkarsh",age:25};
+
+
+// console.log(typeof(x));
+
+
+
+
+let year="fr";
+
+if(year=="may")
+{
+    console.log("The month is may");
+}
+else
+{
+    console.log("THe month is not may");
+}
+
+
+
+// let accessAllowed;
+
+// if(age>18)
+// {
+//     accessAllowed=true;
+// }
+// else
+// {
+//     accessAllowed=false;
+// }
+
+//let result= conditiion ? value1 : value2
+
+// let accessAllowed= (age>18)? true:false
+
+
+
+//loops : while and for 
+
+
+// let i=0;
+// while(i<5)
+// {
+//     alert(i);
+//     i++;
+// }
+
+
+// let i=0;
+
+// do
+// {
+//     alert(i);
+//     i++;
+// }while(i>0);
+
+
+
+for(let i=0;i<3;i++)
+{
+    alert(i);
+}
+
+
+
+
+
---
+++ scriptingFile2.js
@@ -0,0 +1,2 @@
+
+alert("I am coming from a second external file");
