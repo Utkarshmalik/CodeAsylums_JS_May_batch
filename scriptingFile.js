6a3253ec5f5896aa3894c00b5598093ae23a96c1
mohitverma6161 <utkarshmalik06@gmail.com>
2021-05-27 21:32:48 +0530

    this keyword

--- scriptingFile.js
+++ scriptingFile.js
@@ -1,52 +1,19 @@
 
-
-//objects and prototypes
-
-
-//Constructor Functions
+'use strict'
 
 
-//this is used inside of construcotr function : this points to the object/instance of that function
+console.log(this);
 
-function Person(firstName,lastName){
-    this.firstName=firstName;
-    this.lastName=lastName;
+
+var speed=100;
+
+function Car() {
+    this.speed = 120;
 }
 
-Person.prototype.getFullName=function name() {
-    return this.firstName + " " + this.lastName;
-
+Car.prototype.getSpeed = ()=> {
+    return this.speed;
 }
 
-console.log(Person);
-
-
-
-console.log(Person.prototype);
-
-
-     let person1= new  Person("Utkarsh","Malik");
-      let person2=new Person("Rahul","Jain");
-
-      console.log(person1.toString());
-      console.log(person2);
-     
-       console.log(person1.getFullName());
-       console.log(person2.getFullName());
-
-
-       //Person.prototype
-       console.log(person1.__proto__.__proto__);
-
-
-
-
-
-//protypes
-
-
-let arr=[1,2,3,4,5];
-
-console.log(arr.toString());
-
-
+var car = new Car();
+console.log(car.getSpeed());
