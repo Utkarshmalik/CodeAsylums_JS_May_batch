b6872c4a0d100a8eea1034bd2b30b5318a94a59b
mohitverma6161 <utkarshmalik06@gmail.com>
2021-05-28 21:54:04 +0530

    hoisting

--- scriptingFile.js
+++ scriptingFile.js
@@ -1,58 +1,25 @@
+//global context
+//global execution context 
 
 
-//call apply and bind 
-
-
-
-//these are function methods
-
-let cat = {type: 'Cat', sound: 'Meow'};
-let dog = {type: 'Dog', sound: 'Woof'};
-
-let say = function (greeting,anotherGreeting) {
-    console.log(greeting);
-    // access current this
-    console.log(this.type + ' says ' + this.sound);
-};
-
-
-// //first arguemnt in call apply and 
-// //bind tells what should be the value of this inside of that function 
-
-// say('Hello',"Hii");
-// say.call(cat,"Hello","Hiii");
-// say.apply(dog,["Hello","Hii"]);
+//Every time you create a new function a new execution context is created on the top of it 
 
 
 
-// //bind
-//  const newFun=say.bind(cat,"Hii","Hello");
-
-//  newFun();
-
-let car = {
-    speed: 5,
-    start: function() {
-        console.log('Start with ' + this.speed + ' km/h');
-    }
-};
-
-let aircraft = {
-    speed: 10,
-    fly: function() {
-        console.log('Flying');
-    }
-};
-
-
-let startAircraft=car.start.bind(aircraft);
-
-
-startAircraft();
+// x=20;
+// y=10;
+// result;
+// add=undefined;
 
 
 
+let x = 20,
+    y = 10;
 
+let result = add(x,y);
+console.log(result);
 
-
+var add = function(x, y) {
+return x + y;
+}
 
