ed4cbc3c37b6e6a0ba9ff89a3b4c198e8afcf786
mohitverma6161 <utkarshmalik06@gmail.com>
2021-06-02 21:50:31 +0530

    project day 2

--- scriptingFile.js
+++ scriptingFile.js
@@ -1,31 +1,114 @@
 
 
 
+const inputField=document.querySelector("#textField");
+const submitButton=document.querySelector("#submitBtn");
+const IncompleteList=document.querySelector("#incomplete-list");
+const completeList=document.querySelector("#complete-list");
 
 
-function outerMost()
+var incompleteItems=[];
+var completedItems=[];
+
+
+function onMoveButtonClick(e)
 {
-    const name1="Utkarsh";
 
-    function inner()
+    const listId=e.target.parentElement.id;
+
+
+   const completedListItem=incompleteItems.find((element)=>
     {
-        const name2="Rahul";
+        return element.id==listId;
+    })
 
-        function innerMost()
-        {
-            const name3="Rohit";
+    completedItems.push(completedListItem);
 
-            return name1+" "+name2+" "+name3;
-        }
+    incompleteItems=incompleteItems.filter((element)=>
+    {
+        return element.id!==listId;
+    })
 
-        return innerMost;
-    }
-
-    return inner;
+    renderIncompleteList(incompleteItems);
+    renderCompleteList(completedItems);
 }
 
-const partial=outerMost();
-const anotherPartial=partial();
-const data=anotherPartial();
+function onDeleteButtonClick(e)
+{
+    const listId=e.target.parentElement.id;
 
-console.log(data);
+    incompleteItems=incompleteItems.filter((element)=>
+    {
+        return element.id!==listId;
+    })
+
+
+    completedItems=completedItems.filter((element)=>
+    {
+        return element.id!==listId;
+    })
+
+
+
+    renderIncompleteList(incompleteItems);
+    renderCompleteList(completedItems);
+}
+
+function renderIncompleteList(lists)
+{
+    IncompleteList.innerHTML='';
+
+    lists.forEach((listItem)=>
+    {
+        IncompleteList.appendChild(listItem);
+    })
+}
+function renderCompleteList(lists)
+{
+
+    completeList.innerHTML='';
+
+    lists.forEach((listItem)=>
+    {
+        completeList.appendChild(listItem);
+    })
+
+}
+
+
+submitButton.addEventListener("click",()=>
+{
+    const newDiv=document.createElement('div');
+    newDiv.setAttribute("class","list-item");
+
+    const num=Math.floor((Math.random() * 100000) + 1);
+    newDiv.setAttribute("id",num.toString());
+
+
+    const para=document.createElement('p');
+    para.textContent=inputField.value;
+
+    const DeleteButton=document.createElement('button');
+    DeleteButton.setAttribute('class','btn');
+    DeleteButton.textContent="Delete";
+
+    DeleteButton.addEventListener("click",(e)=>onDeleteButtonClick(e));
+
+    const MoveButton=document.createElement('button');
+    MoveButton.setAttribute('class','btn');
+    MoveButton.textContent="Mark as Done";
+    
+    MoveButton.addEventListener("click",(e)=>onMoveButtonClick(e));
+
+    newDiv.appendChild(para);
+    newDiv.appendChild(DeleteButton);
+    newDiv.appendChild(MoveButton);
+    IncompleteList.appendChild(newDiv);
+    incompleteItems.push(newDiv);
+
+})
+
+console.log(incompleteItems);
+
+
+
